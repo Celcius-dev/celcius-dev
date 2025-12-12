c800a8afa596d8ace50bb1272f75d9021b72fa9b
Deniz Can Özder <candenizozder08@gmail.com>
2025-12-12 16:03:00 +0300

    Downgrade React to v18 for compatibility

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,5 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-    svgr(),
-  ],
+  plugins: [react(), svgr()],
 });
