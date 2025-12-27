ed2c7441e636acd6ae1f6089cc09e850a986d1a3
Deniz Can Özder <candenizozder08@gmail.com>
2025-12-27 17:40:32 +0300

    chore: Prepare frontend for Vercel deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Backend adresin
+  baseURL: "https://celcius-dev.onrender.com/api", // Backend adresin
 });
 
 // Her istek gönderilmeden önce araya gir:
